50d906707bc068db5f5126b78e6c1a3de7f54b74
deepanshu <ranadee850@gmail.com>
2026-06-01 01:24:19 +0530

    daruali

--- src/main/java/com/back/entity/Profile.java
+++ src/main/java/com/back/entity/Profile.java
@@ -22,7 +22,12 @@ public class Profile {
     @Column(length = 5000)
     private String about;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(
+            name = "profile_skills",
+            joinColumns = @JoinColumn(name = "profile_id")
+    )
+    @Column(name = "skills")
     private List<String> skills;
 
     @Column(length = 5000)
