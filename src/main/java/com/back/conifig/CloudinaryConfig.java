4a6ed7a4f10dc35d4d8dfc4ce08dbc10d58082a0
deepanshu <ranadee850@gmail.com>
2026-06-01 02:20:21 +0530

    gannnna

--- src/main/java/com/back/conifig/CloudinaryConfig.java
+++ src/main/java/com/back/conifig/CloudinaryConfig.java
@@ -11,15 +11,14 @@ import java.util.Map;
 @Configuration
 public class CloudinaryConfig {
 
-    @Value("${cloudinary.cloud-name}")
+    @Value("${CLOUDINARY_CLOUD_NAME}")
     private String cloudName;
 
-    @Value("${cloudinary.api-key}")
+    @Value("${CLOUDINARY_API_KEY}")
     private String apiKey;
 
-    @Value("${cloudinary.api-secret}")
+    @Value("${CLOUDINARY_API_SECRET}")
     private String apiSecret;
-
     @Bean
     public Cloudinary cloudinary() {
         Map<String,String> map = new HashMap<>();
